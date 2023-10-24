7b529f6669046ffebc705742a884b882d7bea2d4
Vaibhav Bisht <92950515+beastvaibhv@users.noreply.github.com>
2023-10-24 14:02:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 //your JS code here. If required.
+const info = document.getElementById("browser-info");
+info.textContent = "You are using " + navigator.userAgent + navigator.appName + navigator.appVersion;
